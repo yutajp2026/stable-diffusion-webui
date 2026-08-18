77a5cddf45b8ba5b721854730f1581ad52adbf00
yuta <y.takak1234@gmail.com>
2026-08-19 06:19:54 +0900

    changed

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,12 @@
 set PYTHON="C:\Users\%username%\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+
+nvidia-smi
+if %ERRORLEVEL% NEQ 0 (
+    set COMMANDLINE_ARGS=--use-cpu all --precision full --no-half --skip-torch-cuda-test
+) else (
+    set COMMANDLINE_ARGS=--xformers
+)
 
 call webui.bat
